f7afdb8fa5f5e71142c78f35db80cc429469b642
lindanurvitasari <115551133+lindanurvitasari@users.noreply.github.com>
2022-12-02 09:32:32 +0700

    Add files via upload

---
+++ Pertemuan10/src/DatabaseUtilitiesTest/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            KoneksiDatabase.getKoneksi();
+    }
+    
+}
